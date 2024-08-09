1bae6eed62d801a62e4e8b5df729d6b45e6d6afd
TANJIANQUAN <tan472146@gmail.com>
2024-08-09 13:57:59 +0800

    testing

---
+++ src/GitTesting.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author TANJIANQUAN
+ */
+public class GitTesting {
+   public static void main(String[] args) {
+        
+        System.out.println("Hi Iam Tan");
+        System.out.println("GitTsting");
+        
+        // TODO code application logic here
+    }
+    
+}
