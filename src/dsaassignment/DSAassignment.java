d27eac537f37587fba07dd37e6359b1a3c9db821
TANJIANQUAN <tan472146@gmail.com>
2024-08-09 13:36:01 +0800

    testing

---
+++ src/dsaassignment/DSAassignment.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package dsaassignment;
+
+/**
+ *
+ * @author TANJIANQUAN
+ */
+public class DSAassignment {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Hi Iam Tan");
+        
+        // TODO code application logic here
+    }
+    
+}
